4d3a61e72d4c053b6a0cd56309f69d9d30a7b84e
Mars Howard <marsbhoward@gmail.com>
2017-11-20 17:38:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,13 @@ require_relative '../../config/environment'
     end
 
     post '/login' do
-      @user = User.find_by(username: params[:username])
-      if @user.nil?
-        erb :error
-      else
-        session[:user_id] = @user.id
-        redirect to '/account'
-      end
-    end
+      @user = User.find_by(:username => params[:username])
+   if @user != nil && @user.password == params[:password]
+     session[:user_id] = @user.id
+     redirect to '/account'
+   end
+   erb :error
+ end
 
     get '/account' do
       if Helpers.is_logged_in?(session)
